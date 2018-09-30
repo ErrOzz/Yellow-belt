894b20055a377fe33de744d148c01b993db6b9c5
Егор <@192.168.0.196>
2018-09-30 23:57:05 +0500

    First task. Average tempereature. Approved version.

---
+++ w1_c1_temperature/src/w1_c1_temperature.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	cin >> n;
+	vector<int> temps(n);
+	int64_t sum = 0;
+	for (auto& temp : temps) {
+		cin >> temp;
+		sum += temp;
+	}
+	int avg = static_cast<int>(sum / n);
+	set<int> days;
+	int i = 0;
+	for (auto& temp : temps) {
+		if (temp > avg) {
+			days.insert(i);
+		}
+		++i;
+	}
+	cout << days.size() << endl;
+	for (auto& day : days) {
+		cout << day << " ";
+	}
+	return 0;
+}
